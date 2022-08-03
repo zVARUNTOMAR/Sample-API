62b554daab79364524e545e6bd97ba5d5b72a4ee
zvaruntomar <vrnkmr119@gmail.com>
2022-08-03 14:23:13 +0530

    Add project files.

---
+++ Sample API/Controllers/CommonController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace Sample_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommonController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Video> getVideoInfo()
+        {
+             IEnumerable<Video> video = new List<Video>()
+            {
+                new Video() {
+                    Id = 100,
+                    VideoUrl = "https://player.vimeo.com/video/736034733?h=1657d74e59",
+                    Title = "Awesome Birthday"
+                    ,IsRequired = true,
+                },
+                 new Video() {
+                    Id = 200,
+                    VideoUrl = "https://player.vimeo.com/video/736035090?h=43f3c93b42"
+                    ,Title = "Halloween Fun",
+                    IsRequired = false,
+                 },
+                  new Video() {
+                    Id = 300,
+                    VideoUrl = "https://player.vimeo.com/video/735711099"
+                    ,Title = "Christmas Fun",
+                    IsRequired= true
+                  },
+            };
+
+            return video;
+        }
+    }
+}
---
+++ Sample API/Video.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample_API
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string VideoUrl { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
